beacf877c0626e28650d14948a05fddf13dc02ac
dandu574 <41376524+dandu574@users.noreply.github.com>
2018-07-18 19:23:48 +0530

    removed css

--- JD-Assignment/src/App.jsx
+++ JD-Assignment/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './css/App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/CustomNavbar';
